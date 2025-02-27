731547fe9e3d2b163f1cc570c9369a8cf0189949
artamono1 <143677914+artamono1@users.noreply.github.com>
2025-02-27 11:43:49 +0700

    version tic

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def requirements():
 
 configuration = {
     'name': 'druhg',
-    'version': '1.7.3',
+    'version': '1.7.4',
     'description': 'Universal clustering based on dialectical materialism',
     'long_description': readme(),
     'classifiers': [
